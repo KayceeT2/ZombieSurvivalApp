b626024221b77f4c173702124ff753f9906f15f7
Kaycee <KayceeT2>
2021-05-21 09:49:20 -0400

    Initial Commit

---
+++ Zombie Survival App/Controller/ViewController.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var choice2Button: UIButton!
+    @IBOutlet weak var choice1Button: UIButton!
+    @IBOutlet weak var situationLabel: UILabel!
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func choicePressed(_ sender: UIButton) {
+        
+    }
+}
